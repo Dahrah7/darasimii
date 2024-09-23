2c4b58d5bbe3bbc4fbcd2a9ed40f53dcdd40b0b6
Dahrah7 <mobolajioluwadarasimi45@gmail.com>
2024-09-23 04:36:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementById("try").innerHTML ="H96MAX RK3528 ANDROID TV BOX Can store <br> All Your Favourite Media ease .Thanks To <br>the 64Gb eMMC Internal storage <br><br> H96MAX PK3528 ANDROID TV BOX  <br>is manufactured in china and supplied in nigeria<br> by siprotech limited press(x) to see less"
+}
+function hide(){
+    document.getElementById("try").innerHTML ="";
+}
